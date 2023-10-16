8d39043e3e3e1c6eee836986506715348ebce891
x3zus <10615568+x3zus@users.noreply.github.com>
2023-10-16 11:27:40 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt-get update &&
 sudo apt-get upgrade -y &&
-sudo apt-get install screen iperf curl -y &&
+sudo apt-get install screen iperf curl mtr -y &&
 sudo apt autoremove -y
