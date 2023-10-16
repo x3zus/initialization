8986de21201bc24f48778e44d509edc660566dd6
x3zus <10615568+x3zus@users.noreply.github.com>
2023-10-16 11:31:40 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+# Usage: wget https://raw.githubusercontent.com/x3zus/initialization/main/setup.sh && chmod +x setup.sh && ./setup.sh
 sudo apt-get update &&
 sudo apt-get upgrade -y &&
 sudo apt-get install screen iperf curl mtr -y &&
 sudo apt autoremove -y
+rm setup.sh
