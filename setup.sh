a733ba4f6941e988a0a565047912405946c331ad
x3zus <10615568+x3zus@users.noreply.github.com>
2023-11-05 23:48:57 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@
 # Usage: wget https://raw.githubusercontent.com/x3zus/initialization/main/setup.sh && chmod +x setup.sh && ./setup.sh
 sudo apt-get update &&
 sudo apt-get upgrade -y &&
-sudo apt-get install screen iperf curl mtr python-pip -y &&
+sudo apt-get install screen iperf curl mtr python-pip lft -y &&
 sudo apt autoremove -y
 rm setup.sh
