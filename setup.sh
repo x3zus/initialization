5fa3c7232fdcf9702a5632366e4fad14326a090a
x3zus <10615568+x3zus@users.noreply.github.com>
2024-02-23 00:48:18 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 # Usage: wget https://raw.githubusercontent.com/x3zus/initialization/main/setup.sh && chmod +x setup.sh && ./setup.sh
 sudo apt-get update &&
 sudo apt-get upgrade -y &&
-sudo apt-get install screen iperf3 curl mtr python-pip lft nmap git dnsutils -y &&
+sudo apt-get install screen iperf3 curl mtr python3-pip lft nmap git dnsutils -y &&
 sudo apt autoremove -y &&
 curl -s 'https://raw.githubusercontent.com/zerotier/ZeroTierOne/master/doc/contact%40zerotier.com.gpg' | gpg --import && \
 if z=$(curl -s 'https://install.zerotier.com/' | gpg); then echo "$z" | sudo bash; fi
