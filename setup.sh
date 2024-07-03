c1b6857199fd48d8fe9f40592e94248ccd70c0b3
x3zus <10615568+x3zus@users.noreply.github.com>
2024-07-03 13:10:48 -0400

    Update setup.sh
    no zerotier

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ sudo apt-get update &&
 sudo apt-get upgrade -y &&
 sudo apt-get install screen curl mtr python3-pip lft nmap git net-tools dnsutils iperf3 -y &&
 sudo apt autoremove -y &&
-curl -s 'https://raw.githubusercontent.com/zerotier/ZeroTierOne/master/doc/contact%40zerotier.com.gpg' | gpg --import && \
-if z=$(curl -s 'https://install.zerotier.com/' | gpg); then echo "$z" | sudo bash; fi &&
-sudo /sbin/zerotier-cli join 632ea2908594e073 &&
+# curl -s 'https://raw.githubusercontent.com/zerotier/ZeroTierOne/master/doc/contact%40zerotier.com.gpg' | gpg --import && \
+# if z=$(curl -s 'https://install.zerotier.com/' | gpg); then echo "$z" | sudo bash; fi &&
+# sudo /sbin/zerotier-cli join 632ea2908594e073 &&
 rm setup.sh
